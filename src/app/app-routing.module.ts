cefc389f5c51040925387d40290bcc6c7dbdf933
Murat Istek <messi897@gmail.com>
2023-04-06 18:28:06 -0400

    Add "Employee List" component with Bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/employeeList', pathMatch: 'full' },
+  { path: 'employeeList', component: EmployeeListComponent },
+  { path: 'addEmployee', component: EmployeeListComponent },
+  { path: 'addEmployee/:employeeID', component: EmployeeListComponent },
+  { path: 'viewEmployee/:employeeID', component: EmployeeListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { NgbModule, NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,6 +14,7 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule
